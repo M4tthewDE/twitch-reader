d350712f3d66504e4617b6cac5448004664bfa76
M4tthewDE <mattider2@gmail.com>
2021-03-06 11:27:13 +0100

    forgot parser xd

---
+++ main/messageparser.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"log"
+	"strings"
+)
+
+type MessageParser struct {
+	msg_chan chan string
+}
+
+func StartParser(msgParser MessageParser) {
+	for {
+		select {
+		case msg := <-msgParser.msg_chan:
+			if !strings.Contains(msg, ":tmi.twitch.tv") && !strings.Contains(msg, ":justinfan696!justinfan696@justinfan696.tmi.twitch.tv") {
+				i := strings.Index(msg, "#")
+				channel := msg[i:]
+				channel = channel[:strings.Index(channel, " ")]
+				log.Println(channel)
+			}
+		}
+	}
+}
