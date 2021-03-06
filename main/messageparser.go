6fda9ea59e0394df4561d01ccf87af39ad979ca8
M4tthewDE <mattider2@gmail.com>
2021-03-06 11:29:22 +0100

    fix linter

--- main/messageparser.go
+++ main/messageparser.go
@@ -10,15 +10,12 @@ type MessageParser struct {
 }
 
 func StartParser(msgParser MessageParser) {
-	for {
-		select {
-		case msg := <-msgParser.msg_chan:
-			if !strings.Contains(msg, ":tmi.twitch.tv") && !strings.Contains(msg, ":justinfan696!justinfan696@justinfan696.tmi.twitch.tv") {
-				i := strings.Index(msg, "#")
-				channel := msg[i:]
-				channel = channel[:strings.Index(channel, " ")]
-				log.Println(channel)
-			}
+	for msg := range msgParser.msg_chan {
+		if !strings.Contains(msg, ":tmi.twitch.tv") && !strings.Contains(msg, ":justinfan696!justinfan696@justinfan696.tmi.twitch.tv") {
+			i := strings.Index(msg, "#")
+			channel := msg[i:]
+			channel = channel[:strings.Index(channel, " ")]
+			log.Println(channel)
 		}
 	}
 }
